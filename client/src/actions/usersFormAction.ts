a36705813c40bb0b3ec68b5a1ee16b8ce0e1fd2c
VladlenGorbunov <gorbunov.vladlen2014@gmail.com>
2021-12-22 19:37:35 +0300

    Протестирован usersFormAction

--- client/src/actions/usersFormAction.ts
+++ client/src/actions/usersFormAction.ts
@@ -2,7 +2,7 @@ import { AxiosResponse } from 'axios';
 import { Dispatch } from 'redux';
 import { UsersFormAC, UsersFormACTypes } from '../types/redux/usersForm';
 import { fetchUsersForm } from '../utils/fetchLocalServer';
-import { LOADING_EMULATION_TIME } from '../constants/common';
+// import { LOADING_EMULATION_TIME } from '../constants/common';
 import HttpStatuses from '../constants/httpStatuses';
 
 const loadUsersFormAC = (page: number, limit: number) => async (dispatch: Dispatch<UsersFormAC>) => {
@@ -19,14 +19,14 @@ const loadUsersFormAC = (page: number, limit: number) => async (dispatch: Dispat
 
     if (response.status === HttpStatuses.OK) {
       const users = await response.data;
-      setTimeout(() => {
-        dispatch({
-          type: UsersFormACTypes.LOAD_USERS_FORM_SUCCESS,
-          payload: {
-            data: users.data, total: users.total, page: users.page, limit: users.limit
-          }
-        });
-      }, LOADING_EMULATION_TIME);
+      // setTimeout(() => {
+      dispatch({
+        type: UsersFormACTypes.LOAD_USERS_FORM_SUCCESS,
+        payload: {
+          data: users.data, total: users.total, page: users.page, limit: users.limit
+        }
+      });
+      // }, LOADING_EMULATION_TIME);
     } else {
       throw new Error(`${response.status.toString()} – ${response.data.error.message}`);
     }
